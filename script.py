dd8a0c68bf36be5ea9d3d5af007c27e8150ebe8d
ndowen5545 <33293104+ndowen5545@users.noreply.github.com>
2017-11-02 08:44:33 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,26 +1,62 @@
 # #!/usr/bin/env python
-#from IPy import IP
+import getpass
 
-tcpinput = ['NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL']
-#           sfile   specip  specpt    sip    dip     sport   dport   dfile
+current_user = getpass.getuser()
+tcpinput = ['/home/' + current_user + '/packets-master/', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '/tmp/tcpoutput.pcap']
+#                              sdir                       specip  specpt    sip    dip     sport   dport           dfile
 
 banner = "\n\n\n          ~~~~~~~~~~~~~~~~~~~~ TCPDUMP PARCER ~~~~~~~~~~~~~~~~~~~~"
-menu_text = """This script is designed to allow tcpdump to parce through multiple
-PCAP files as well as simplify the tcpdump command.
+def menu_text():
+  print """This script is designed to allow tcpdump to parce through multiple PCAP files as 
+well as simplify the tcpdump command.
 
 If you do not want to search for a certain field, leave empty.
 There is no need to input any data for non-essential fields.
+By default, the source file path is the 'packets-master/' directory within 
+    the current user's home directory.
+By default, the destination file is 'tcpoutput.pcap' in the '/tmp/' directory.
+
+              --------------Current configuration-------------
+"""
+
+  print " Source File Path: ", tcpinput[0]
+  print "  Dest. File Path: ", tcpinput[7], "\n"
+  for i in range(len(tcpinput)):
+    if not tcpinput[i] == 'NULL':
+      if i == 1:
+        print "               IP: ", tcpinput[1]
+      elif not i == 1:
+        if i == 3:
+          print "        Source IP: ", tcpinput[3]
+        if i == 4:
+          print "   Destination IP: ", tcpinput[4]
+      if i == 2:
+        print "             Port: ", tcpinput[2]
+      elif not i == 2:
+        if i == 5:
+          print "      Source Port: ", tcpinput[5]
+        if i == 6:
+          print " Destination Port: ", tcpinput[6]
+  print """
+              ------------------------------------------------
 
 If a specific IP or port is made, source/destination IPs/ports will be nulled.
 
 Please choose from the following options:
+  Modify:
     1) Specific IP          4) Specific Port
     2) Source IP            5) Source Port 
     3) Destination IP       6) Destination Port
     
     7) Source File          8) Destination File
     
-    D) Display current input
+  Clear:
+    C1) Specific IP          C4) Specific Port
+    C2) Source IP            C5) Source Port 
+    C3) Destination IP       C6) Destination Port
+    
+    C7) Source File          C8) Destination File
+    
     E) Execute tcpdump with current configuration
     Q) Quit script
 """
@@ -100,28 +136,10 @@ def validate_port(s):
   else:
     return True
 
-def displayer():
-  for i in range(len(tcpinput)):
-          if not tcpinput[i] == 'NULL':
-            if i == 1:
-              print "               IP: ", tcpinput[1]
-            elif not i == 1:
-              if i == 3:
-                print "        Source IP: ", tcpinput[3]
-              if i == 4:
-                print "   Destination IP: ", tcpinput[4]
-            if i == 2:
-              print "             Port: ", tcpinput[2]
-            elif not i == 2:
-              if i == 5:
-                print "      Source Port: ", tcpinput[5]
-              if i == 6:
-                print " Destination Port: ", tcpinput[6]
-
 def main():
   while True:
     print banner
-    print menu_text
+    menu_text()
     user = raw_input(">")
     
     if user == "1" or user == "2" or user == "3":
@@ -129,14 +147,29 @@ def main():
     elif user == "4" or user == "5" or user == "6":
       port_func(user)
     
-#   elif user == "7":
+    #elif user == "7":
       
-    
-    elif user == "D" or user == "d":
-      print "\n\nCurrent running configuration:"
-      displayer()
-#   elif user == "E" or user == "e":
-#     tcpd_com = "for i in `find", tcpinput[0], "-type f` ; do tcpdump -n -r $i port 22 -w $i'.tmp'; done && mergecap  -w test3.pcap *.tmp && find -type f -name '*tmp*' -delete"
+      
+    elif user == "E" or user == "e":
+      tcpd_com = "for i in `find " + str(tcpinput[0]) + " -type f` ; do tcpdump -n -r $i" 
+      for i in range(len(tcpinput)):
+        if not tcpinput[i] == 'NULL':
+          if i == 1:
+            tcpd_com += " host " + str(tcpinput[1])
+          elif not i == 1:
+            if i == 3:
+              tcpd_com += " src host " + str(tcpinput[3])
+            if i == 4:
+              tcpd_com += " dst host " + str(tcpinput[4])
+          if i == 2:
+            tcpd_com += " port " + str(tcpinput[2])
+          elif not i == 2:
+            if i == 5:
+              tcpd_com += " src port " + str(tcpinput[5])
+            if i == 6:
+              tcpd_com += " dst port " + str(tcpinput[6])
+      tcpd_com += " -w /tmp/$i'.tmpdump'; done && mergecap  -w " + str(tcpinput[7]) + " /tmp/*.tmpdump && find -type f -name /tmp/'*tmpdump*' -delete"
+      print tcpd_com
     elif user == "Q" or user == "q":
       break
     else:
